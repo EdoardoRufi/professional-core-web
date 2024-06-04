daecc58ef8153a37e14617945c1f10c83a3ed588
Rufi, Edoardo (contracted) <Edoardo.Rufi@contracted.pmi.com>
2024-06-04 17:26:41 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <TheNavbar :types="types"></TheNavbar>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import TheNavbar from './components/layouts/TheNavbar.vue';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      types: ['Landscape', 'Engineering', 'Architecture', 'Planning']
+    } 
+  },
+  components: {
+    HelloWorld,
+    TheNavbar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/layouts/TheNavbar.vue
@@ -0,0 +1,41 @@
+<template>
+    <header>
+        <h1>
+            <span v-for="type in types" :key="type"  @click="filterList(type)"> {{ type }} </span>
+        </h1>
+    </header>
+</template>
+
+<script>
+export default {
+    props: ['types'],
+    methods: {
+        filterList(type) {
+            //add the logic to filter the list depending by type value
+            console.log(type)
+        }
+    }
+}
+</script>
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: center; /* Centra orizzontalmente il contenuto */
+    align-items: center; /* Centra verticalmente il contenuto all'interno dell'altezza della navbar */
+    background-color: black; /* Colore di sfondo nero */
+    color: white; /* Testo bianco */
+    height: 60px; /* Altezza della navbar */
+    width: 100%; /* Larghezza completa della pagina */
+    position: fixed; /* Fissa la navbar in alto */
+    top: 0; /* Posiziona la navbar in alto */
+    left: 0; /* Allinea la navbar al bordo sinistro */
+}
+
+h1 {
+    display: flex;
+    gap: 100px; /* Aggiunge uno spazio tra le stringhe, opzionale */
+    margin: 0; /* Rimuove il margine predefinito */
+    font-size: 16px; /* Imposta la dimensione del testo a 16px (puoi modificare il valore come desideri) */
+}
+</style>
