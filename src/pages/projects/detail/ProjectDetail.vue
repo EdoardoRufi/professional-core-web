30def2610663742419310e986a7211fa85b72a71
Rufi, Edoardo (contracted) <Edoardo.Rufi@contracted.pmi.com>
2024-06-21 17:02:25 +0200

    wip
    detailItem unnecessary?

--- src/pages/projects/detail/ProjectDetail.vue
+++ src/pages/projects/detail/ProjectDetail.vue
@@ -1,27 +1,19 @@
 <template>
   <div class="project-detail">
-    <!-- Main Image -->
-    <p>blabla {{ id }}</p>
     <!-- Horizontal List of Detail Images -->
     <div class="details-container">
       <ul class="horizontal-list">
         <!--TODO: uncomment TheProject and try to make it work!-->
-        <!--<li>
-          <TheProject :id="id" :isMain="true" />
-        </li>-->
-        <!--<li v-for="detail in projectDetails" :key="detail.order">
-          <TheProject
-            :id="id"
-            :isMain="false"
-            :order="detail.order"
-          />
-        </li>-->
+        <!-- the project <ProjectDetailItem :id="id" order="000"/>-->
+        <the-project :id="id" :isMain="false"></the-project>
+        <the-project v-for="detail in projectDetails" :key="detail.order" :id="id" :order="detail.order" :isMain="false" />
       </ul>
-    </div> 
+    </div>
   </div>
 </template>
-  
+
 <script>
+import TheProject from '../TheProject.vue';
 //import TheProject from '../TheProject.vue';
 
 export default {
@@ -29,12 +21,12 @@ export default {
   computed: {
     projectDetails() {
       const project = this.$store.getters['projects/projects'].find(project => project.id === parseInt(this.id));
-      console.log(project.details);
+      console.log("id: " + this.id + " details " + project.details[0] + "project:" + project[0]);
       return project ? project.details : [];
     }
   },
   components: {
-   //TheProject,
+    TheProject
   }
 };
 </script>
@@ -49,6 +41,26 @@ export default {
   overflow-x: auto;
   white-space: nowrap;
   padding: 1rem 0;
+  scrollbar-width: thin; /* For Firefox */
+  scrollbar-color: #888 #eee; /* For Firefox */
+  width: 100%;
+  max-width: 1400px; /* Set a larger max-width for the container */
+  margin: auto;
+}
+
+/* For Webkit-based browsers (Chrome, Safari) */
+.details-container::-webkit-scrollbar {
+  height: 12px;
+}
+
+.details-container::-webkit-scrollbar-track {
+  background: #eee;
+}
+
+.details-container::-webkit-scrollbar-thumb {
+  background-color: #888;
+  border-radius: 10px;
+  border: 3px solid #eee;
 }
 
 .horizontal-list {
@@ -60,5 +72,30 @@ export default {
 
 .horizontal-list li {
   margin-right: 1rem;
+  flex: 0 0 20%; /* Adjust the width of each item */
+  box-sizing: border-box; /* Include padding and border in the element's total width and height */
 }
+
+.card-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.image img {
+  width: 100%;
+  height: auto;
+  object-fit: cover;
+}
+
+h3 {
+  font-size: 1.0rem;
+  margin: 0.5rem 0;
+}
+
+p {
+  margin: 0.25rem 0;
+  /* Adjusted margin for a more compact look */
+}
+
 </style>
